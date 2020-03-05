6dd5b65aa4bdebf81ed97232610147c556004a69
Louis-dev <louis.nillus@gmail.com>
2020-03-06 00:06:04 +0100

    Update 2

--- Assets/ObstacleBehaviour.cs
+++ Assets/ObstacleBehaviour.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Sirenix.OdinInspector;
 
 public class ObstacleBehaviour : MonoBehaviour
 {
-    [Range(0,100)]
+    [Range(0,1000), HideIf("isBoss", true)]
     public int hp;
     int initialhp;
+    [Range(0, 1000), ShowIf("isBoss", true)]
+    public int bossHp;
 
     [Range(0,10)]
     public float speed;
@@ -17,18 +20,46 @@ public class ObstacleBehaviour : MonoBehaviour
 
     public Image hpBar;
 
+    public bool isBoss;
+
+    [ShowIf("isBoss", true)]
+    public MiniBoss miniBoss;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Projectile")
         {
-            hp -= collision.gameObject.GetComponent<ProjectileBehaviour>().dmg;
-            if(hp <= 0)
+            if(this.tag == "Obstacle")
             {
-                Destroy(this.gameObject);
+                hp -= collision.gameObject.GetComponent<ProjectileBehaviour>().dmg;
+                if(hp <= 0)
+                {
+                    Destroy(this.gameObject);
+                }
             }
+
+            if(this.tag == "MiniBoss")
+            {
+                bossHp -= collision.gameObject.GetComponent<ProjectileBehaviour>().dmg;
+                if (miniBoss.hp <= 0)
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+
         }
         else if(collision.gameObject.tag == "Player")
         {
+            collision.gameObject.GetComponent<PlayerController>().hp--;
+            StartCoroutine(collision.gameObject.GetComponent<PlayerController>().SetImmunity(1f));
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player" && (miniBoss.fatalRange/100f * miniBoss.hp) > initialhp)
+        {
+            Debug.Log("Boss is not enough damaged");
             collision.gameObject.GetComponent<PlayerController>().hp--;
             StartCoroutine(collision.gameObject.GetComponent<PlayerController>().SetImmunity(1f));
         }
@@ -38,6 +69,12 @@ public class ObstacleBehaviour : MonoBehaviour
     void Start()
     {
         initialhp = hp;
+
+        if(this.tag == "MiniBoss")
+        {
+            initialhp = miniBoss.hp;
+        }
+
         rb = this.GetComponent<Rigidbody2D>();
 
         switch(direction)
@@ -61,9 +98,13 @@ public class ObstacleBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(hpBar.fillAmount != 1f / initialhp * hp)
+        if(hpBar.fillAmount != 1f / initialhp * hp && this.tag == "Obstacle")
         {
             hpBar.fillAmount = 1f / initialhp * hp;
+        }
+        else if(this.tag == "MiniBoss" && hpBar.fillAmount != 1f / initialhp * miniBoss.hp)
+        {
+            hpBar.fillAmount = 1f / initialhp * miniBoss.hp;
         }
     }
 }
